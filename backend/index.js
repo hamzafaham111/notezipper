bfeeeb82a878eb1b1704587c5bb259d4b31db74f
Hamza <hamzafaham111@gmail.com>
2021-12-27 08:22:10 -0800

    first commet of noteZipper which is just the folder sturucture of the project

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const notes = require('./data/notes');
+const dotenv = require('dotenv');
+const app = express();
+app.get("/", (req, res) => {
+    res.send("API Is Running")
+})
+
+app.get("/api/notes", (req, res) => {
+    res.send(notes);
+})
+
+app.get("/api/notes/:id", (req, res) => {
+    const note = notes.find((e) => e._id === req.params.id)
+    res.send(note);
+})
+const PORT = process.env.PORT || 5000;
+app.listen(5000, () => {
+    console.log(`Server is running on port ${PORT}`)
+});
